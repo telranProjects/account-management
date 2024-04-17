893e6b1b4b346153a1e4a0291e5891e91be0a59c
Georgy Levin <g.phyzy@gmail.com>
2024-04-17 11:38:29 +0300

    version with @Autowired AccountRepo

---
+++ src/test/java/telran/security/accounting/test/SecurityConfigurationTest.java
@@ -0,0 +1,143 @@
+package telran.security.accounting.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import telran.security.accounting.dto.Roles;
+import telran.security.accounting.dto.AccountData;
+import telran.security.accounting.model.AccountDataDoc;
+import telran.security.accounting.repo.AccountRepo;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SecurityConfigurationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	AccountRepo accountRepo;
+	
+	@Value("${app.security.user.list.url}")
+	private String userGetAllUrl;
+	@Value("${app.security.user.c.d.url}")
+	private String userCDUrl;
+	
+	String requestBody =  "{\"username\": \"username@username.il\", \"password\": \"password\", \"roles\": [\"USER\"]}";
+	
+	String username = "username@username.il";
+	String password = "password";
+	Roles[] roles = new Roles[]{Roles.ADMIN, Roles.USER};
+	AccountData accountData = new AccountData(username, password, roles);
+	
+	AccountDataDoc accountDoc = AccountDataDoc.builder()
+			.username(username)
+			.hashPassword(password)
+			.roles(roles) 
+			.build();
+
+	@Test
+	void testUserRole() throws Exception {
+		
+		String role = "USER";
+		
+		String url = userGetAllUrl;
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get(url);		
+		int status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.FORBIDDEN.value(), status);
+		
+		url = userGetAllUrl;
+		request = MockMvcRequestBuilders.post(url);		
+		status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.FORBIDDEN.value(), status);
+		
+		url = userGetAllUrl;
+		request = MockMvcRequestBuilders.delete(url);		
+		status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.FORBIDDEN.value(), status);		
+		
+		url = userCDUrl;
+		request = MockMvcRequestBuilders.post(url);		
+		status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.FORBIDDEN.value(), status);
+		
+		url = userCDUrl;
+		request = MockMvcRequestBuilders.delete(url);		
+		status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.FORBIDDEN.value(), status);
+		
+		url = userCDUrl;
+		request = MockMvcRequestBuilders.get(url);		
+		status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.FORBIDDEN.value(), status);
+
+	}
+
+	int getResponseStatus(MockHttpServletRequestBuilder request, String role) throws Exception {
+
+		return mockMvc.perform(request
+				.with(SecurityMockMvcRequestPostProcessors.user("user").roles(role))
+				.contentType(MediaType.APPLICATION_JSON))
+				.andReturn().getResponse().getStatus();
+	}
+	
+	@Test
+	void testAdminRole() throws Exception {
+		
+		String role = "ADMIN";
+		
+		String url = userCDUrl;
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(url)
+				.contentType(MediaType.APPLICATION_JSON)
+		        .content(requestBody);
+		int status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.FORBIDDEN.value(), status);		
+		
+		request = MockMvcRequestBuilders.delete(url + "/" + username);
+		status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.FORBIDDEN.value(), status);		
+		
+		
+		url = userGetAllUrl;
+		request = MockMvcRequestBuilders.get(url);		
+		status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.FORBIDDEN.value(), status);
+	}
+	
+	@Test
+	void testSURole() throws Exception {
+		
+		String role = "SU";		
+		
+		String url = userCDUrl;
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(url)
+				.contentType(MediaType.APPLICATION_JSON)
+		        .content(requestBody);
+		int status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.OK.value(), status);
+		
+		accountRepo.save(accountDoc);
+		request = MockMvcRequestBuilders.delete(url + "/" + username);
+		status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.OK.value(), status);		
+		
+		
+		url = userGetAllUrl;
+		request = MockMvcRequestBuilders.get(url);		
+		status = getResponseStatus(request, role);
+		assertEquals(HttpStatus.OK.value(), status);
+	}
+	
+}
+
